74df66068bac6679aecb995a0c5361ee3a0c69a6
Evan Stokdyk <evan.stokdyk@gmail.com>
2022-08-30 14:25:49 -0700

    Wrote Code

--- src/Account.java
+++ src/Account.java
@@ -1,4 +1,17 @@
 
 public class Account {
 
+	Integer id;
+	double balence;
+	
+	public Account (Integer id) {
+		this.id = id;
+		this.balence = 0.0;
+	}
+	
+	public Account (Integer id, double balence) {
+		this.id = id;
+		this.balence = balence;
+	}
+	
 }
