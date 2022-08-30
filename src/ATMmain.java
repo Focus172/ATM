d253e5c5efb7e6ee43d2e373659789ee9f536472
Evan Stokdyk <evan.stokdyk@gmail.com>
2022-08-30 13:59:27 -0700

    first things

---
+++ src/ATMmain.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class ATMmain {
+	
+	HashMap idMap = new HashMap();
+	
+	public openAccount (int id) {
+		
+	}
+	
+	public openAccount (int id, double balence) {
+		
+	}
+}
